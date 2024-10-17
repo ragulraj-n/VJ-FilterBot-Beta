df303a6131764727ba007e7d252145e8eaadbd06
ragulraj-n <135788153+ragulraj-n@users.noreply.github.com>
2024-10-17 20:28:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ MELCOW_VID = environ.get("MELCOW_VID", "https://t.me/How_To_Open_Linkl")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
 
 # Admins, Channels & Users
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001909657560'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002061061565'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6632419944').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
