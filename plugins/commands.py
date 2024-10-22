70418f0419d0b7ca21fa4d60520d8eb2503c0d2f
ragulraj-n <135788153+ragulraj-n@users.noreply.github.com>
2024-10-22 17:29:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -73,7 +73,7 @@ async def start(client, message):
         if CLONE_MODE == True:
             buttons.append([InlineKeyboardButton('🤖 Cʀᴇᴀᴛᴇ Yᴏᴜʀ Oᴡɴ Cʟᴏɴᴇ Bᴏᴛ 🤖', callback_data='clone')])
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("CAACAgUAAxkBAAEKVaxlCWGs1Ri6ti45xliLiUeweCnu4AACBAADwSQxMYnlHW4Ls8gQMAQ") 
+        m=await message.reply_sticker("CAACAgUAAxkBAAICBmcXkt_dTJZMNDrwH7zCorzkc605AAICEQACVb4YVkfLNnccdt9FHgQ") 
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
@@ -210,7 +210,7 @@ async def start(client, message):
             if CLONE_MODE == True:
                 buttons.append([InlineKeyboardButton('🤖 Cʀᴇᴀᴛᴇ Yᴏᴜʀ Oᴡɴ Cʟᴏɴᴇ Bᴏᴛ 🤖', callback_data='clone')])
             reply_markup = InlineKeyboardMarkup(buttons)
-            m=await message.reply_sticker("CAACAgUAAxkBAAEKVaxlCWGs1Ri6ti45xliLiUeweCnu4AACBAADwSQxMYnlHW4Ls8gQMAQ") 
+            m=await message.reply_sticker("CAACAgUAAxkBAAICBmcXkt_dTJZMNDrwH7zCorzkc605AAICEQACVb4YVkfLNnccdt9FHgQ") 
             await asyncio.sleep(1)
             await m.delete()
             await message.reply_photo(
